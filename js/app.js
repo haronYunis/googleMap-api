3283bf8f44b7d3826d87763a19b3b247d089e86a
haronYunis <haronyunis@icloud.com>
2018-05-05 21:15:18 -0700

    added js file to hold my map function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log("Connected LIVE!")
+var map;
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+  });
+}
+
+
+console.log("Connected!!!");
